138a45f36646014c61c2d4824f27c4a63d91ab73
IcyAzureDrake <cal2160956@maricopa.edu>
2024-11-28 23:03:05 -0700

    The gnome has been shaken, not stirred.

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -4,12 +4,37 @@ public class SortingUtility {
 
 
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
-
+        int pos = 0;
+        while (pos < data.length){
+            if (pos == 0 || data[pos-1].compareTo(data[pos]) <= 0){
+                pos ++;
+            }
+            else {
+                swap(data , pos, pos-1);
+                pos --;
+            }
+        }
         // TODO implement Gnome Sort here
     }
 
 
     public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
+        boolean swapped;
+        int i;
+        do {
+            swapped = false;
+
+            for (i = 0; i < data.length - 1; i++){
+                if (data[i].compareTo(data[i+1]) > 0){
+                    swap(data, i, i+1);
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+
+        } while (swapped);
+
 
         // TODO implement Cocktail Shaker Sort here
     }
